ff75437299b1650dd70f4c0a2575e8b4219c5eab
mdrowshansheikh <mdrowshansheikh008@gmail.com>
2022-10-31 22:28:27 +0600

    cart list delete cart and price counting

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { useRouter } from 'next/router';
+
 import data from '../../utils/data';
 import Layout from '../../components/layout';
 import Link from 'next/link';
 import Image from 'next/image';
 import { Store } from '../../utils/Store';
 import { useContext } from 'react';
+import { useRouter } from 'next/router';
 
 const ProductScreen = () => {
   const { state, dispatch } = useContext(Store);
+  const router = useRouter();
 
   const { query } = useRouter();
   const { slug } = query;
@@ -30,19 +32,20 @@ const ProductScreen = () => {
     }
 
     dispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity } });
+    router.push('/cart');
   };
 
   // Done ========================= end
   return (
-    <Layout title={product.name}>
+    <Layout title={product?.name}>
       <div className="py-2">
         <Link href="/">back to products</Link>
       </div>
       <div className="grid md:grid-cols-4 md:gap-3">
         <div className="md:col-span-2">
           <Image
-            src={product.image}
-            alt={product.name}
+            src={product?.image}
+            alt={product?.name}
             width={640}
             height={640}
             layout="responsive"
@@ -51,25 +54,27 @@ const ProductScreen = () => {
         <div>
           <ul>
             <li>
-              <h1 className="text-lg">{product.name}</h1>
+              <h1 className="text-lg">{product?.name}</h1>
             </li>
-            <li>Category: {product.category}</li>
-            <li>Brand: {product.brand}</li>
+            <li>Category: {product?.category}</li>
+            <li>Brand: {product?.brand}</li>
             <li>
-              {product.rating} of {product.numReviews} reviews
+              {product?.rating} of {product?.numReviews} reviews
             </li>
-            <li>Description: {product.description}</li>
+            <li>Description: {product?.description}</li>
           </ul>
         </div>
         <div>
           <div className="card p-5">
             <div className="mb-2 flex justify-between">
               <div>Price</div>
-              <div>${product.price}</div>
+              <div>${product?.price}</div>
             </div>
             <div className="mb-2 flex justify-between">
               <div>Status</div>
-              <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
+              <div>
+                {product?.countInStock > 0 ? 'In stock' : 'Unavailable'}
+              </div>
             </div>
             <button
               className="primary-button w-full"
