44c146f6954803b1f15caf58fab1611f4aec8ad6
mdrowshansheikh <mdrowshansheikh008@gmail.com>
2022-10-31 08:56:03 +0600

    Cart Details

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const ProductItem = ({ product }) => {
   return (
     <div className="card">
-      <Link legacyBehavior href={'/product/${product.slug}'}>
+      <Link legacyBehavior href={`/product/${product.slug}`}>
         <a>
           <img
             src={product.image}
@@ -14,7 +14,7 @@ const ProductItem = ({ product }) => {
         </a>
       </Link>
       <div className="flex flex-col items-center justify-center p-5 ">
-        <Link legacyBehavior href={'/product/${product.slug}'}>
+        <Link legacyBehavior href={`/product/${product.slug}`}>
           <a>
             <h2 className="text-lg">{product.name}</h2>
           </a>
